27b6b95dae451129437c80645bbfaf856f45908a
dmitri-baklan <lapota2015@gmail.com>
2021-08-31 16:35:12 +0300

    Clear gitignore

---
+++ hw2/src/main/java/com/company/Controller.java
@@ -0,0 +1,74 @@
+package main.java.com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//import java.com.company.Model;
+//import java.com.company.View;
+
+//import com.company.Model;
+//import com.company.View;
+
+public class Controller {
+
+    Model model;
+    View view;
+
+
+    ArrayList<Object> attempts = new ArrayList<Object>();
+
+
+    public Controller (Model model, View view){
+        this.model = model;
+        this.view = view;
+    }
+
+    public void startGame(){
+        int input = -1;             /*  */
+        model.generateRandNumber();
+
+
+        do{
+            Scanner sc = new Scanner(System.in);
+            view.printMessage(view.GREETING_MESSAGE);
+            view.displayCurrentBoundaries(model.getMinBoundary(), model.getMaxBoundary());
+            view.printMessage(view.USER_INPUTS);
+            view.displayAttempts(attempts);
+
+
+            //view.printMessage(Integer.toString(model.getX_value()));
+
+            if(!sc.hasNextInt()){
+                view.printMessage(view.INCORRECT_INPUT_MESSAGE);
+                continue;
+            }
+            input = sc.nextInt();
+
+            if(!model.checkInputInRange(input)){
+                view.printMessage(view.OUT_OF_RANGE_MESSAGE);
+                continue;
+            }
+
+
+            attempts.add(input);
+
+            if(model.checkWin(input)){
+                view.printMessage(view.WINNING_MESSAGE + model.getX_value());
+                return;
+            }
+            if(model.checkIsBiggerThanX(input)){
+                model.setMaxBoundary(input);
+                view.printMessage("\""+input+"\""+view.INPUT_BIGGER_MESSAGE);
+                continue;
+            }
+            else{
+                model.setMinBoundary(input);
+                view.printMessage("\""+input+"\""+view.INPUT_LESS_MESSAGE);
+                continue;
+            }
+
+        }while(!model.checkWin(input));
+
+    }
+
+}
---
+++ hw2/src/main/java/com/company/Model.java
@@ -0,0 +1,56 @@
+package main.java.com.company;
+
+import org.junit.Test;
+
+public class Model {
+
+    private final int RANGE_MIN = 0;
+    private final int RANGE_MAX = 100;
+
+    private int dynamic_range_min = this.RANGE_MIN;          // boundaries that change based on input
+    private int dynamic_range_max = this.RANGE_MAX;
+    private static  int x_value;                              //value that user try to guess
+
+
+    public void generateRandNumber(){
+        this.x_value = (int) (Math.random()*(this.RANGE_MAX+1)+this.RANGE_MIN);         // [0; 100] boundaries include
+    }
+
+    @Test
+    public boolean checkWin(int input){
+        return this.x_value == input;
+    }
+
+    public boolean checkInputInRange(int input){
+        if(input < dynamic_range_min || input > dynamic_range_max){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkIsBiggerThanX(int input){
+        return (input > this.x_value);
+    }
+
+    public static int getX_value() {
+        return x_value;
+    }
+
+    public void setMinBoundary(int min){
+        this.dynamic_range_min = min;
+    }
+
+    public void setMaxBoundary(int max){
+        this.dynamic_range_max = max;
+    }
+
+    public int getMinBoundary(){
+        return this.dynamic_range_min;
+    }
+
+    public int getMaxBoundary(){
+        return this.dynamic_range_max;
+    }
+
+
+}
