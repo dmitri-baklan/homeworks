253440e8bdbda550c35cd29c8187b687e03dd863
dmitri-baklan <84872570+dmitri-baklan@users.noreply.github.com>
2021-09-10 09:48:03 +0300

    Update Main.java

--- ImmutableObject/src/Main.java
+++ ImmutableObject/src/Main.java
@@ -2,6 +2,13 @@ public class Main {
 
 
     public static void main(String[] args) {
+        Age age = new Age();
+        age.setYear(10);
+        ImmutableStudent immutableStudent = new ImmutableStudent("Viktor",age);
+        ImmutableStudent immutableStudent1 = immutableStudent.setName("PAvel");
+        age.setYear(12);
+        ImmutableStudent immutableStudent2 = immutableStudent.setAge(age);
+        System.out.println(immutableStudent+" "+immutableStudent1+" "+immutableStudent2);
 
     }
 
