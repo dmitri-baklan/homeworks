3bf6e111812edbf2d31d3842e8145d7b852432e1
dmitri-baklan <lapota2015@gmail.com>
2021-09-10 12:02:46 +0300

    ImmutableObject main run

---
+++ immutable/src/Age.java
@@ -0,0 +1,7 @@
+
+public class Age {
+    private int  year;
+    public int getYear(){return this.year;}
+    public void setYear(int year){this.year = year;}
+
+}
---
+++ immutable/src/ImmutableStudent.java
@@ -0,0 +1,23 @@
+public final class ImmutableStudent {
+
+    private final String name;
+    private final Age age;
+
+    public ImmutableStudent(String name, Age age){
+        this.name = name;
+        Age cloneAge = new Age();
+        cloneAge.setYear(age.getYear());
+        this.age = cloneAge;
+    }
+
+    public String getName(){return this.name;}
+    public Age getAge(){
+        Age cloneAge = new Age();
+        cloneAge.setYear(this.age.getYear());
+        return cloneAge;
+    }
+    public ImmutableStudent setName(String name){return new ImmutableStudent(name, this.age);}
+    public ImmutableStudent setAge(Age age){return new ImmutableStudent(this.name, age);}
+
+}
+
